3d5af4993cbd6afd2d017b56d720cb16c0d96a3f
Rico <d-Rickyy-b@users.noreply.github.com>
2023-12-10 02:10:53 +0100

    feat: use argparse for cli switches

--- cmd/kdbxParser/main.go
+++ cmd/kdbxParser/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/akamensky/argparse"
 	"kdbxParser/pkg/kdbx"
 	"log"
 	"os"
 )
 
 func main() {
-	targetFile := flag.String("file", "", "Path to kdbx file")
-	flag.Parse()
+	parser := argparse.NewParser("kdbxParser", "Obtain metadata from a KeePass file")
+	parser.ExitOnHelp(true)
+
+	targetFile := parser.String("f", "file", &argparse.Options{Required: true, Help: "Path to the keepass file"})
+
+	if err := parser.Parse(os.Args); err != nil {
+		fmt.Print(parser.Usage(err))
+		os.Exit(1)
+	}
 
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module kdbxParser
 
 go 1.21
 
-require github.com/google/uuid v1.4.0 // indirect
+require (
+	github.com/akamensky/argparse v1.4.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+)
